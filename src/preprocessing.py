214a0cf7ed56d01e83bb51784ec1f8c55e33044b
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-08-21 22:26:49 +0700

    update docker compose

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,4 +4,6 @@ import pandas as pd
 
 df = pd.read_excel(settings.input_path, sheet_name='Transactions')
 # print(df.head())
-print(df.columns)
+# print(df.columns)
+data_dict = df.to_dict()
+print(data_dict)
