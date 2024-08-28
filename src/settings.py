a850add5125512b19c56261a10b1c0c4c4fa7541
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-08-28 17:56:30 +0700

    update data shortening

--- src/settings.py
+++ src/settings.py
@@ -1,1 +1,9 @@
+from os import getenv
+
 input_path = 'input_data/CoffeeShopSalesTmp.xlsx'
+
+MONGO_HOST = getenv("MONGO_HOST", "localhost")
+MONGO_PORT = int(getenv("MONGO_PORT", 0)) or 27017
+MONGO_USER = getenv("MONGO_USER", "")
+MONGO_PASSWORD = getenv("MONGO_PASSWORD", "")
+MONGO_DB_NAME = getenv("MONGO_DB_NAME", "data_warehouse")
--- src/shorten.py
+++ src/shorten.py
@@ -5,6 +5,7 @@ df = pd.read_excel(settings.input_path, sheet_name='Transactions')
 
 data_dict = df.iloc[1:].to_dict(orient='records')
 
-
 print(data_dict[:5])
-print(len(data_dict))
+
+shortened_df = df.head(1000)
+shortened_df.to_excel("CoffeeShopSalesTmp.xlsx", index=False)
