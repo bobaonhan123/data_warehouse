9a2dbd0a9e8efd7536e15b665d79e626cf5e0641
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-09-13 18:57:03 +0700

    import sales by time

--- src/import_data/import_sales_by_time.py
+++ src/import_data/import_sales_by_time.py
@@ -3,6 +3,7 @@ from src import settings
 from src.db import db
 
 sales_by_time_db = db["sales_by_time"]
+time_db = db["time"]
 
 def import_sales_by_time():
     try:
@@ -10,6 +11,11 @@ def import_sales_by_time():
             sales_by_time_db.drop()
     except:
         pass
+    sales_by_time_db.create_index(
+    [('product_id', 1), ('year', 1), ('month', 1), ('weekday', 1), ('day', 1), ('hour', 1)],
+    unique=True
+    )
+    
     
     df = pd.read_excel(settings.input_path, sheet_name='Transactions')
 
@@ -26,8 +32,37 @@ def import_sales_by_time():
     
     # Convert to dictionary
     data_dict = df.to_dict(orient='records')
+    ndict={}
+    for data in data_dict:
+        id_and_time = str(data['product_id']) +"-"+ str(data['year']) +"-"+ str(data['month']) +"-"+ str(data['weekday']) +"-"+ str(data['day']) +"-"+ str(data['hour'])
+        if id_and_time in ndict:
+            ndict[id_and_time]['transaction_qty']+=data['transaction_qty']
+        else:
+            ndict[id_and_time]=data
+    data_id = 1
+    inserted = []
+    for data in ndict.values():
+        try:
+            transaction_datetime = pd.Timestamp(data['transaction_date'].strftime('%Y-%m-%d') + ' ' + data['transaction_time'].strftime('%H:%M:%S'))
+            time_dimension = time_db.find_one({'year': data['year'], 'month': data['month'], 'weekday': data['weekday'], 'day': data['day'], 'hour': data['hour']})
+            time_dim_id = time_dimension['time_id']
+            data_to_insert = {
+                'sales_by_time_id': data_id,
+                'product_id': data['product_id'],
+                'quantity': data['transaction_qty'],
+                'unit_price': data['unit_price'],
+                'time_dim_id': time_dim_id,
+                'transaction_datetime': transaction_datetime,
+            }
+            sales_by_time_db.insert_one(data_to_insert)
+            inserted.append(data_to_insert)
+            
+            data_id += 1
+        except:
+            pass
+    
     
 
-    print(data_dict[:5])
-    print(len(data_dict))
+    print(inserted[:5])
+    print("sales_by_time",len(inserted))
     
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,9 +2,11 @@ from import_data import import_time
 from .import_data import import_product
 from .import_data import import_store
 from .import_data import import_prod_in_store
+from .import_data import import_sales_by_time
 
 
 import_time.import_time()
 import_store.import_store()
 import_product.import_product()
 import_prod_in_store.import_prod_in_store()
+import_sales_by_time.import_sales_by_time()
