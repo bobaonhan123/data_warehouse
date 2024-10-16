691f96f531ffd0aca4a8bfbacb48e1324e2c89d6
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-10-16 13:05:50 +0700

    measure time

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from import_data import import_time
 from .import_data import import_product
 from .import_data import import_store
@@ -6,9 +7,14 @@ from .import_data import import_sales_by_time
 from .import_data import import_transaction
 
 
+start_time = datetime.now()
+
 import_time.import_time()
 import_store.import_store()
 import_product.import_product()
 import_prod_in_store.import_prod_in_store()
 import_sales_by_time.import_sales_by_time()
 import_transaction.import_transaction()
+
+time_taken = datetime.now() - start_time
+print("Time taken: ", time_taken)
